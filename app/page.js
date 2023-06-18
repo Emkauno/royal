03d77f397e6100da25a6a17fcc5766968f301c26
Emkauno <emkauno@gmail.com>
2023-06-18 07:46:18 -0400

    styled components

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>La mejor</h1>
+      <h1>La msejor</h1>
     </main>
   )
 }
+  
